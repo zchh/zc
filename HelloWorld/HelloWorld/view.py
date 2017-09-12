d3a664c3b688925190519ca0ebd1fa93464397ec
zc <zc123zhangchi@gmail.com>
2017-09-12 16:11:53 +0800

    初始化项目

---
+++ HelloWorld/HelloWorld/view.py
@@ -0,0 +1,19 @@
+# from django.http import HttpResponse
+#
+#
+# def hello(request):
+#     return HttpResponse("Hello world ! ")
+
+# -*- coding: utf-8 -*-
+
+# from django.http import HttpResponse
+
+
+
+from django.shortcuts import render
+
+
+def hello(request):
+    context = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'hello.html', context)
